37b79a1f109838fc70fa2738219e2ad37177cfbc
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-11-10 13:41:36 +0300

    Added inner classes

---
+++ src/com/tsekhanovich/masterclass/innerclasses/Playlist.java
@@ -0,0 +1,17 @@
+package com.tsekhanovich.masterclass.innerclasses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Pavel Tsekhanovich 09.11.2018
+ */
+
+public class Playlist {
+
+    private List<Album> albums;
+
+    public Playlist() {
+        this.albums = new ArrayList<>();
+    }
+}

e57fb57eec1f48923dfd605e9a71ec7c537c75ae
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-11-11 21:18:38 +0300

    Added oop classes

---
+++ src/com/tsekhanovich/masterclass/oop/classes/lecture36/Main.java
@@ -0,0 +1,15 @@
+package com.tsekhanovich.masterclass.oop.classes.lecture36;
+
+/**
+ * @author Pavel Tsekhanovich 09.11.2018
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("Carrera");
+    }
+}

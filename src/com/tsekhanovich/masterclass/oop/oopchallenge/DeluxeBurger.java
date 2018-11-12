e11bc8b0bc65cfb234d06152f3e9237bef058e32
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-11-12 11:05:14 +0300

    Added oop challenge and polymorphism

---
+++ src/com/tsekhanovich/masterclass/oop/oopchallenge/DeluxeBurger.java
@@ -0,0 +1,34 @@
+package com.tsekhanovich.masterclass.oop.oopchallenge;
+
+/**
+ * @author Pavel Tsekhanovich 09.11.2018
+ */
+
+public class DeluxeBurger extends Hamburger {
+
+    public DeluxeBurger() {
+        super("Deluxe", "Sausage and Bacon", 14.54, "White");
+        super.addHamburgerAddition1("Chips", 2.75);
+        super.addHamburgerAddition2("Drink", 1.81);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+}
---
+++ src/com/tsekhanovich/masterclass/oop/polymorphism/Main.java
@@ -0,0 +1,37 @@
+package com.tsekhanovich.masterclass.oop.polymorphism;
+
+/**
+ * @author Pavel Tsekhanovich 09.11.2018
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            Movie movie = randomMovie();
+            System.out.println("Movie #" + i + ": " + movie.getName());
+            System.out.println("Plot: " + movie.plot());
+            System.out.println();
+        }
+    }
+
+    public static Movie randomMovie() {
+        int random = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated: " + random);
+
+        switch (random) {
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgetable();
+        }
+
+        return null;
+    }
+}
